f18cde8d150a6157edf9bf697d10a05dc2c5d066
prathameshanabhavane <pratham291994@gmail.com>
2021-05-20 12:38:49 +0530

    revert file

--- js/main.js
+++ js/main.js
@@ -1,26 +1,11 @@
 // Make sure service wotker are supported
 
-function serviceWorker() {
-    if ('serviceWorker' in navigator) {
-        // console.log('Service Worker Supported');
-        window.addEventListener('load', () => {
-            navigator.serviceWorker
-                .register('../service-worker.js')
-                .then(reg => console.log('Service Worker: Registered'))
-                .catch(err => console.log(`Service Worker: Error: ${err}`))
-        })
-    }
+if ('serviceWorker' in navigator) {
+    // console.log('Service Worker Supported');
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('../service-worker-two.js')
+            .then(reg => console.log('Service Worker: Registered'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`))
+    })
 }
-
-
-window.addEventListener("load", (event) => {
-  // const statusDisplay = document.querySelector("body");
-  // statusDisplay.textContent = navigator.onLine ? "Online" : "OFFline";
-  if (navigator.onLine) {
-      console.log("online");
-      serviceWorker();
-  } else {
-    console.log("offline");
-    serviceWorker();
-  }
-});
