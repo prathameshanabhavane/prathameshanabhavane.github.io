40dfa88098e8f81867366d471ccedca878839a05
prathameshanabhavane <pratham291994@gmail.com>
2021-05-20 12:11:45 +0530

    load on offline service worker and cache it

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 // Make sure service wotker are supported
 
-if ('serviceWorker' in navigator) {
-    // console.log('Service Worker Supported');
-    window.addEventListener('load', () => {
-        navigator.serviceWorker
-            .register('../service-worker-two.js')
-            .then(reg => console.log('Service Worker: Registered'))
-            .catch(err => console.log(`Service Worker: Error: ${err}`))
-    })
+function serviceWorker() {
+    if ('serviceWorker' in navigator) {
+        // console.log('Service Worker Supported');
+        window.addEventListener('load', () => {
+            navigator.serviceWorker
+                .register('../service-worker.js')
+                .then(reg => console.log('Service Worker: Registered'))
+                .catch(err => console.log(`Service Worker: Error: ${err}`))
+        })
+    }
 }
+
+
+window.addEventListener("load", (event) => {
+  // const statusDisplay = document.querySelector("body");
+  // statusDisplay.textContent = navigator.onLine ? "Online" : "OFFline";
+  if (navigator.onLine) {
+      console.log("online");
+      serviceWorker();
+  } else {
+    console.log("offline");
+    serviceWorker();
+  }
+});
